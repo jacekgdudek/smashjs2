696d5e3679a830f8bcd802782285063baf0a4c42
Jacek Dudek <jacek@peekabu.net>
2013-03-05 16:13:47 +0000

    added delay after hints on tutorial scene, added volume control over click for the tutorial

--- web/js/scenes/tutorialScene.js
+++ web/js/scenes/tutorialScene.js
@@ -15,6 +15,7 @@ var tutorialHintType = {
 var tutorialScene = (function() {
 
 	var currentHint, hints, delay, startRotation/* for relative roation conditions*/ , volumeTarget, wentPastTarget;
+	var hintDelay, hintDelayCounter; /*for controlling delay between hints*/ 
 
 	var currentNumber, lastNumber;
 
@@ -36,6 +37,8 @@ var tutorialScene = (function() {
 
 			sweet_spot = this.scene.sweet_spot;
 			this.currentHint = 0;
+			this.hintDelay = 20;
+			this.hintDelayCounter = 0;
 
 			// Setup the game stage
 			knob = this.scene.visuals[1];
@@ -103,193 +106,203 @@ var tutorialScene = (function() {
 			//hide GUI
 			hideGUI();
 
-			// verbouse
-			if(this.lastHint != this.currentHint)
+			if(this.hintDelayCounter > this.hintDelay)
 			{
-				console.log("Current Hint : " + this.currentHint);
-				this.lastHint = this.currentHint;
-			}
 
-
-			// ---------------------------------Check if we have finished
-			if (this.currentHint >= this.hints.length) {
-				console.log("Combination Found!");
-				addEventEx(this.scene.nextStageEvent);
-				return;
-				// Aww yeah!
-			}
-
-			//---------------------------------------------update input
-			var directionNow = false;
-			var directionChanged = false;
-			if(!this.hints[this.currentHint].fid0_disabled)
-			{
-				stethoscope.bitmap.x = 800 - stethoscope.bitmap.image.width - (((800 - stethoscope.bitmap.image.width)/(640 - (2*100)))*(inputArray[0].x - 100));
-				stethoscope.bitmap.y = (600/480)*inputArray[0].y;
-			}
-			if(!this.hints[this.currentHint].fid2_disabled)
-			{
-				var rotation = inputArray[2].rotation;
-				// Keep this.input rotation within bounds
-				if (rotation > 360) { rotation = 360; }
-				if (rotation <= -360) { rotation = -360; }
-
-				// Make the dial spin if we win the game
-				var spinSpeed = 9;
-				var spinDuration = 400;
-				knobNumbers.bitmap.rotation = rotation;
-
-				//---------get currentNumber based on rotation
-				this.currentNumber = Math.floor((knobNumbers.maxNumber/360)*rotation);
-				//---------check if changed direction on the number
-				if(this.currentNumber != this.lastNumber)
+				// verbouse
+				if(this.lastHint != this.currentHint)
 				{
-					directionNow = (this.currentNumber > this.lastNumber);
-					if(this.direction != directionNow)
+					console.log("Current Hint : " + this.currentHint);
+					this.lastHint = this.currentHint;
+					hintDelayCounter = 0;
+				}
+
+
+				// ---------------------------------Check if we have finished
+				if (this.currentHint >= this.hints.length) {
+					console.log("Combination Found!");
+					addEventEx(this.scene.nextStageEvent);
+					return;
+					// Aww yeah!
+				}
+
+				//---------------------------------------------update input
+				var directionNow = false;
+				var directionChanged = false;
+				if(!this.hints[this.currentHint].fid0_disabled)
+				{
+					stethoscope.bitmap.x = 800 - stethoscope.bitmap.image.width - (((800 - stethoscope.bitmap.image.width)/(640 - (2*100)))*(inputArray[0].x - 100));
+					stethoscope.bitmap.y = (600/480)*inputArray[0].y;
+				}
+				if(!this.hints[this.currentHint].fid2_disabled)
+				{
+					var rotation = inputArray[2].rotation;
+					// Keep this.input rotation within bounds
+					if (rotation > 360) { rotation = 360; }
+					if (rotation <= -360) { rotation = -360; }
+
+					// Make the dial spin if we win the game
+					var spinSpeed = 9;
+					var spinDuration = 400;
+					knobNumbers.bitmap.rotation = rotation;
+
+					//---------get currentNumber based on rotation
+					this.currentNumber = Math.floor((knobNumbers.maxNumber/360)*rotation);
+					//---------check if changed direction on the number
+					if(this.currentNumber != this.lastNumber)
 					{
-						console.log("Changed Direction!");
-						directionChanged = true;
-						this.direction = directionNow;
+						directionNow = (this.currentNumber > this.lastNumber);
+						if(this.direction != directionNow)
+						{
+							console.log("Changed Direction!");
+							directionChanged = true;
+							this.direction = directionNow;
+						}
 					}
 				}
-			}
 
-			//------------------------------------ update volume
-			//distance from sweet spot
-			var distance = Math.sqrt(Math.pow(this.sweet_spot.x - stethoscope.bitmap.x,2) + Math.pow(this.sweet_spot.y - stethoscope.bitmap.y,2));
-			if (distance > 100) distance = 100;
-			this.volume = distance;
+				//------------------------------------ update volume
+				//distance from sweet spot
+				var distance = Math.sqrt(Math.pow(this.sweet_spot.x - stethoscope.bitmap.x,2) + Math.pow(this.sweet_spot.y - stethoscope.bitmap.y,2));
+				if (distance > 100) distance = 100;
+				audioManager.setVolume((100-distance)/100, audioManagerAudioObject.NORMAL_CLICK);
+				this.volume = distance;
 
-			//---------------------------------------------process Hint
-			switch(this.hints[this.currentHint].successCondition._type)
-			{
-				//--DELAY
-				case 0:
-					//set delay if not set
-					if( this.delay == 9999 ) this.delay = this.hints[this.currentHint].successCondition._value;
-					// -- delay
-					this.delay--;
-					//if delay finnished
-					if( this.delay < 0 )
-					{
-						this.delay = 9999;
-						this.currentHint++;
-					}
-				break;
-				//--Fiducial was updated
-				case 1:
-					//check if was updated
-					if( inputArray[this.hints[this.currentHint].successCondition._value].wasUpdated == true )
-					{
-						this.currentHint++;
-					}
-				break;
-				//--relative rotation greater than
-				case 2:
-					//set start rotation if not set
-					if(this.startRotation == 9999) this.startRotation = knobNumbers.bitmap.rotation;
-					if(directionNow && this.startRotation + this.hints[this.currentHint].successCondition._value)
-					{
-						startRotation = 9999;
-						this.currentHint++;
-					}
-				break;
-				//--relative rotation lesser than
-				case 3:
-					//set start rotation if not set
-					if(this.startRotation == 9999) this.startRotation = knobNumbers.bitmap.rotation;
-					if(!directionNow && this.startRotation - this.hints[this.currentHint].successCondition._value)
-					{
-						startRotation = 9999;
-						this.currentHint++;
-					}
-				break;
-				//--change direction
-				case 4:
-					//set start rotation if not set
-					if(directionChanged) 
-					{
-						this.currentHint++;
-					}
-				break;
-				//--change direction at number
-				case 5:
-					//set start rotation if not set
-					if(directionChanged && (this.currentNumber >= this.hints[this.currentHint].successCondition._value-1 ||
-												 this.currentNumber <= this.hints[this.currentHint].successCondition._value+1)) 
-					{
-						this.currentHint++;
-					}
-				break;
-				//--get to number
-				case 6:
-					//set start rotation if not set
-					if(this.currentNumber == this.hints[this.currentHint].successCondition._value) 
-					{
-						console.log("Got to number");
-						this.currentHint++;
-					}
-				break;
-				//--volume over
-				case 7:
-					//set volume target if not set
-					console.log("distance : " + this.volume);
-					if(this.volumeTarget == 9999) this.volumeTarget = this.hints[this.currentHint].successCondition._value;
-					if(this.volume < this.volumeTarget)
-					{
-						this.volumeTarget = 9999;
-						this.currentHint++;
-					}
-				break;
-			}
-
-			//--------------------------------------------------updateMessage
-			if(typeof this.hints[this.currentHint].message !== 'undefined')
-			{
-				if(this.hints[this.currentHint].message !=  this.scene.messages[0].text.text)
+				//---------------------------------------------process Hint
+				switch(this.hints[this.currentHint].successCondition._type)
 				{
-					this.scene.stage.removeChild(this.scene.messages[0].bg)
-					changeText(this.hints[this.currentHint].message , this.scene.messages[0]);
-					this.scene.messages[0].bg.visible = true;
-					this.scene.messages[0].text.visible = true;
-					this.scene.stage.addChild(this.scene.messages[0].bg)
+					//--DELAY
+					case 0:
+						//set delay if not set
+						if( this.delay == 9999 ) this.delay = this.hints[this.currentHint].successCondition._value;
+						// -- delay
+						this.delay--;
+						//if delay finnished
+						if( this.delay < 0 )
+						{
+							this.delay = 9999;
+							this.currentHint++;
+						}
+					break;
+					//--Fiducial was updated
+					case 1:
+						//check if was updated
+						if( inputArray[this.hints[this.currentHint].successCondition._value].wasUpdated == true )
+						{
+							this.currentHint++;
+						}
+					break;
+					//--relative rotation greater than
+					case 2:
+						//set start rotation if not set
+						if(this.startRotation == 9999) this.startRotation = knobNumbers.bitmap.rotation;
+						if(directionNow && this.startRotation + this.hints[this.currentHint].successCondition._value)
+						{
+							startRotation = 9999;
+							this.currentHint++;
+						}
+					break;
+					//--relative rotation lesser than
+					case 3:
+						//set start rotation if not set
+						if(this.startRotation == 9999) this.startRotation = knobNumbers.bitmap.rotation;
+						if(!directionNow && this.startRotation - this.hints[this.currentHint].successCondition._value)
+						{
+							startRotation = 9999;
+							this.currentHint++;
+						}
+					break;
+					//--change direction
+					case 4:
+						//set start rotation if not set
+						if(directionChanged) 
+						{
+							this.currentHint++;
+						}
+					break;
+					//--change direction at number
+					case 5:
+						//set start rotation if not set
+						if(directionChanged && (this.currentNumber >= this.hints[this.currentHint].successCondition._value-1 ||
+													 this.currentNumber <= this.hints[this.currentHint].successCondition._value+1)) 
+						{
+							this.currentHint++;
+						}
+					break;
+					//--get to number
+					case 6:
+						//set start rotation if not set
+						if(this.currentNumber == this.hints[this.currentHint].successCondition._value) 
+						{
+							console.log("Got to number");
+							this.currentHint++;
+						}
+					break;
+					//--volume over
+					case 7:
+						//set volume target if not set
+						console.log("distance : " + this.volume);
+						if(this.volumeTarget == 9999) this.volumeTarget = this.hints[this.currentHint].successCondition._value;
+						if(this.volume < this.volumeTarget)
+						{
+							this.volumeTarget = 9999;
+							this.currentHint++;
+						}
+					break;
 				}
-				//make invisible if empt
-			}
 
-			// -----------------------If we hit the target move on to the next one
-			var currentTarget = this.listOfTargets[this.targetPointer];
-
-			//--------------------------update ALTMessage
-			if(typeof this.hints[this.currentHint].messageEx !== 'undefined')
-			{
-				for(var i = 0 ; i < hints[this.currentHint].messageEx.length ; i++)
+				//--------------------------------------------------updateMessage
+				if(typeof this.hints[this.currentHint].message !== 'undefined')
 				{
-					switch(this.hints[this.currentHint].messageEx[i].condition.type)
+					if(this.hints[this.currentHint].message !=  this.scene.messages[0].text.text)
 					{
-						case tutorialHintType.FAR_FROM_NUMBER_AFTER_CLICK:
-							var distance = (currentNumber > currentTarget ? currentNumber-currentTarget : currentTarget - currentNumber);
-							if(this.wentPastTarget && distance > this.hints[this.currentHint].messageEx[i].condition._value)
-							{
-								this.message = hints[this.currentHint].message;
-							}
-						break;
+						this.scene.stage.removeChild(this.scene.messages[0].bg)
+						changeText(this.hints[this.currentHint].message , this.scene.messages[0]);
+						this.scene.messages[0].bg.visible = true;
+						this.scene.messages[0].text.visible = true;
+						this.scene.stage.addChild(this.scene.messages[0].bg)
+					}
+					//make invisible if empt
+				}
+
+				// -----------------------If we hit the target move on to the next one
+				var currentTarget = this.listOfTargets[this.targetPointer];
+
+				//--------------------------update ALTMessage
+				if(typeof this.hints[this.currentHint].messageEx !== 'undefined')
+				{
+					for(var i = 0 ; i < hints[this.currentHint].messageEx.length ; i++)
+					{
+						switch(this.hints[this.currentHint].messageEx[i].condition.type)
+						{
+							case tutorialHintType.FAR_FROM_NUMBER_AFTER_CLICK:
+								var distance = (currentNumber > currentTarget ? currentNumber-currentTarget : currentTarget - currentNumber);
+								if(this.wentPastTarget && distance > this.hints[this.currentHint].messageEx[i].condition._value)
+								{
+									this.message = hints[this.currentHint].message;
+								}
+							break;
+						}
 					}
 				}
-			}
 
-			//trigger sound if number changed
-			if (this.currentNumber != this.lastNumber)
+				//trigger sound if number changed
+				if (this.currentNumber != this.lastNumber)
+				{
+					console.log(this.currentNumber);
+					audioManager.playSound(audioManagerAudioObject.NORMAL_CLICK);
+				}
+
+				//save last values
+				this.lastNumber = this.currentNumber;
+
+				//update scene
+				this.scene.stage.update();
+			}
+			else
 			{
-				console.log(this.currentNumber);
-				audioManager.playSound(audioManagerAudioObject.NORMAL_CLICK);
+				this.hintDelayCounter++;
 			}
-
-			//save last values
-			this.lastNumber = this.currentNumber;
-
-			//update scene
-			this.scene.stage.update();
 		},
 		finalize: function() {
 			for(var i = 0 ; i < this.scene.visuals.length ; i++)
